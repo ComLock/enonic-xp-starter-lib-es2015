a02448d35e2fa0e2d780530623525c2d5bbbf2d0
Christian Andreas Westgaard <Christian.Westgaard@gjensidige.no>
2018-08-16 12:21:53 +0200

    One tab is more efficient than four spaces

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,66 +1,67 @@
 module.exports = {
 
-    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
-    extends: 'airbnb-base',
+	// https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+	extends: 'airbnb-base',
 
-    globals: {
+	globals: {
 
-        // Nashorn
-        Java: false,
+		// Nashorn
+		Java: false,
 
-        // Enonic XP
-        app: false,
-        log: false,
-        resolve: false,
-        __: false,
+		// Enonic XP
+		app: false,
+		log: false,
+		resolve: false,
+		__: false,
 
-        // Mocha
-        //describe: false,
-        //it: false,
+		// Mocha
+		//describe: false,
+		//it: false,
 
-        // Client-side js
-        //console: false,
-        //document: false,
-        //window: false,
+		// Client-side js
+		//console: false,
+		//document: false,
+		//window: false,
 
-        // Jquery
-        //$: false,
-        //jQuery: false,
+		// Jquery
+		//$: false,
+		//jQuery: false,
 
-    },
-    rules: { // https://eslint.org/docs/rules
-        'comma-dangle': ['error', {
-            arrays: 'ignore',
-            objects: 'never',
-            imports: 'never',
-            exports: 'never',
-            functions: 'ignore'
-        }],
-        'import/extensions': ['off'],
-        'import/prefer-default-export': ['off'],
-        'import/no-absolute-path': ['off'],
-        'import/no-extraneous-dependencies': ['off'],
-        'import/no-unresolved': ['off'],
-        indent: ['error', 4],
-        'max-len': ['error', 100, 2, {
-            ignoreUrls: true,
-            ignoreComments: true,
-            ignoreRegExpLiterals: true,
-            ignoreStrings: true,
-            ignoreTemplateLiterals: true,
-        }],
-        'no-cond-assign': ['error', 'except-parens'],
-        'no-multi-spaces': ['off'],
-        'no-underscore-dangle': ['error', {
-            allow: [
-                '_path',
-                '_id'
-            ],
-            allowAfterThis: false,
-            allowAfterSuper: false,
-            enforceInMethodNames: false,
-        }],
-        'object-curly-spacing': ['off'],
-        'spaced-comment': ['off']
-    } // rules
+	},
+	rules: { // https://eslint.org/docs/rules
+		'comma-dangle': ['error', {
+			arrays: 'ignore',
+			objects: 'never',
+			imports: 'never',
+			exports: 'never',
+			functions: 'ignore'
+		}],
+		'import/extensions': ['off'],
+		'import/prefer-default-export': ['off'],
+		'import/no-absolute-path': ['off'],
+		'import/no-extraneous-dependencies': ['off'],
+		'import/no-unresolved': ['off'],
+		indent: ['warn', 'tab'],
+		'max-len': ['error', 100, 2, {
+			ignoreUrls: true,
+			ignoreComments: true,
+			ignoreRegExpLiterals: true,
+			ignoreStrings: true,
+			ignoreTemplateLiterals: true,
+		}],
+		'no-cond-assign': ['error', 'except-parens'],
+		'no-multi-spaces': ['off'],
+		'no-tabs': ['off'],
+		'no-underscore-dangle': ['error', {
+			allow: [
+				'_path',
+				'_id'
+			],
+			allowAfterThis: false,
+			allowAfterSuper: false,
+			enforceInMethodNames: false,
+		}],
+		'object-curly-spacing': ['off'],
+		'spaced-comment': ['off']
+	} // rules
 }; // exports
